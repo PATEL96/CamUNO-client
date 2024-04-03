c85df8635f71bbdf0fe69d01fe80cc4fc4bd7be8
PATEL96 <patel.raj.official.96@gmail.com>
2024-04-03 20:05:20 +0530

    client: Added Discord Login

--- src/app/context/SocketProvider.tsx
+++ src/app/context/SocketProvider.tsx
@@ -16,8 +16,8 @@ const SocketContext = createContext<SocketContextType>({ socket: null });
 
 export const useSocket = () => useContext(SocketContext);
 
-// const socket = io("http://134.209.155.223:5000");
-const socket = io("http://localhost:5000");
+const socket = io("http://134.209.155.223:5000");
+// const socket = io("http://localhost:5000");
 // const socket = io('http://192.168.0.102:5000');
 // const socket = io('http://192.168.0.103:5000');
 
